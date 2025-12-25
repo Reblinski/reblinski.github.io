504421e469db55f7fd1751c89f1b03c1f19a91a5
rysiek <ryszard.duda@frakoterm.pl>
2025-12-25 18:19:17 +0100

    Naprawa błędu lintera w konfiguracji

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/', // Bardzo ważne dla reblinski.github.io
+  base: "/", // Bardzo ważne dla reblinski.github.io
   build: {
-    outDir: 'dist',
-  }
-})
+    outDir: "dist",
+  },
+});
